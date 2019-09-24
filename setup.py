7233c41f4d39fa9bb5dd51ba3cd20a474c9f95ac
Stanislav <svohnik@yahoo.com>
2019-09-23 18:18:59 -0700

    readme exaples added

--- setup.py
+++ setup.py
@@ -14,19 +14,32 @@ setup(
     version='0.1.0',  # Required
     description='tools for daily usage',  # Optional
     url='https://github.com/hombre66/hombre_tools',  # Optional
-    classifiers=[
-        'Development Status :: 5 - Production',
-        'Intended Audience :: DB Developers',
-        'Topic :: Databases',
-        'License ::  MIT License',
-        'Programming Language :: Python :: 3.7',
-    ],
 
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
 
     python_requires='>=3.6',
-
-
+    install_requires=['cx-Oracle==7.2.2',
+                      'JayDeBeApi==1.1.1',
+                      'jupyter==1.0.0',
+                      'numpy==1.17.0',
+                      'pandas==0.25.0',
+                      'pandas-profiling==2.3.0',
+                      'pyodbc==4.0.27',
+                      'pyOpenSSL==19.0.0',
+                      'pyperclip==1.7.0',
+                      'python-dateutil==2.8.0',
+                      'python-settings==0.2',
+                      'pywin32==224',
+                      'pywin32-ctypes==0.2.0',
+                      'pywinpty==0.5.5',
+                      'requests==2.22.0',
+                      'scipy==1.3.0',
+                      'seaborn==0.9.0',
+                      'SQLAlchemy==1.3.7',
+                      'sqlparse==0.3.0',
+                      'tables==3.5.2',
+                      'urllib3==1.25.3',
+                      'zipp==0.5.2'],
     extras_require={  # Optional
         'dev': ['check-manifest'],
         'test': ['coverage'],
