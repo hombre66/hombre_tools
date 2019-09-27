ff5ae4fba4984a4a0c689fb9eabcd8cfb27f9508
Stanislav <svohnik@yahoo.com>
2019-09-26 19:45:58 -0700

    Pasword renamed to pswd

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md')) as f:
 setup(
     name='hombre_tools',  # Required
 
-    version='0.1.16',  # Required
+    version='0.1.17',  # Required
     description='tools for daily usage',  # Optional
     url='https://github.com/hombre66/hombre_tools',  # Optional
 
